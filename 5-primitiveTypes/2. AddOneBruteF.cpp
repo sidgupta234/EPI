68af8ff6cf7d448d60cd8bf8f0544159d86bf567
Siddharth Gupta <siddharthgupta234@gmail.com>
2016-06-10 12:04:02 +0530

    Create 2. AddOneBruteF.cpp

---
+++ 5-primitiveTypes/2. AddOneBruteF.cpp
@@ -0,0 +1,33 @@
+// Write a program which takes as inpit an array of digits encoding a decimal number D and updates the array to represent the number D +1.
+// Example <1,2,9> -> <1,3,0>
+#include <bits/stdc++.h>
+using namespace std;
+
+int deleteDuplicatesFromSortedArray(vector <int> &values);
+
+int main(){
+	vector <int> values;
+	int a[]={2,2,2,2,3,3,3,3,4,4,4,4,4,5,5,5};
+	
+	values.insert(values.end(), a, a+sizeof(a)/sizeof(a[0]));
+	int numOfValues=deleteDuplicatesFromSortedArray(values);
+
+	for(int i=0;i<numOfValues;i++){
+		cout<<values[i]<<' ';
+	}
+
+	return 0;
+}
+
+int deleteDuplicatesFromSortedArray(vector <int> &values){
+	int writeIndex =1, repeats=0;
+
+	for(int i=1;i<values.size();i++){
+		
+		if(values[i]!=values[i-1]){
+			values[writeIndex]=values[i];
+			writeIndex++;
+		}
+	}
+	return writeIndex;	
+}
